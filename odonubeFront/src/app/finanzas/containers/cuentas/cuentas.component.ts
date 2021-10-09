a4a0e1f67e9263e2eeed60ddd89330158c712750
jabbcode <jabbsontouch@gmail.com>
2021-10-09 19:12:57 -0400

    modificando interfaz de modulo finanzas, registrar vauche, ver vauches y registrar cuenta

--- odonubeFront/src/app/finanzas/containers/cuentas/cuentas.component.ts
+++ odonubeFront/src/app/finanzas/containers/cuentas/cuentas.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { FinanzasService } from '../../services/finanzas.service';
 
 @Component({
   selector: 'app-cuentas',
@@ -6,10 +17,104 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuentas.component.scss']
 })
 export class CuentasComponent implements OnInit {
+  @Input()
+  editMode: number;
+  @Input()
+  usuario: any;
 
-  constructor() { }
+  @Output()
+  onData = new EventEmitter<any>();
 
-  ngOnInit(): void {
+  registroCuenta: FormGroup;
+
+  showError: {};
+  errorMsg: string;
+
+  constructor(
+    private auth: AuthService,
+    private cuentas: FinanzasService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.initForm();
+    this.showError = {
+      errorAct: false,
+    };
+  }
+
+  initForm() {
+    this.registroCuenta = new FormGroup({
+      idcuenta: new FormControl('', Validators.required),
+      name: new FormControl('', Validators.required),
+      typecuenta: new FormControl('', Validators.required),
+      status: new FormControl('', Validators.required)
+    });
+  }
+  get fCuenta() {
+    return this.registroCuenta.controls;
+  }
+
+  async endRegistro() {
+    try {
+      var aux = this.registroCuenta.value;
+      let error;
+
+      let cuenta = {
+        idcuenta: aux.idcuenta,
+        name: aux.name,
+        typecuenta: aux.typecuenta,
+        status: aux.status
+      };
+
+      error = this.catchCuentaErrors();
+      if (error) {
+        let errorMsg =
+          'Algunos campos son inválidos. Por favor, revise el formulario e intente de nuevo';
+        this.openError(errorMsg);
+      } else {
+        let response = await this.cuentas.create(cuenta);
+        console.log(response);
+        this.router.navigateByUrl("cuentas/lista")
+      }
+    } catch (e: any) {
+      this.openError(e.message);
+    }
+  }
+
+  catchCuentaErrors() {
+    let aux1 = this.fCuenta.idcuenta.errors
+      ? this.fCuenta.idcuenta.errors.required
+      : false;
+    let aux2 = this.fCuenta.name.errors
+      ? this.fCuenta.name.errors.required
+      : false;
+    let aux3 = this.fCuenta.typecuenta.errors
+      ? this.fCuenta.typecuenta.errors.required
+      : false;
+    let aux4 = this.fCuenta.status.errors
+      ? this.fCuenta.status.errors.required
+      : false;
+    let error = aux1 || aux2 || aux3 || aux4;
+    return error;
+  }
+
+  openError(msg: string) {
+    this.errorMsg = msg;
+    this.showError = {
+      errorAct: true,
+    };
+  }
+
+  closeError() {
+    this.showError = {
+      errorAct: false,
+    };
+  }
+
+  resetForms() {
+    this.registroCuenta.reset();
   }
 
 }
--- odonubeFront/src/app/finanzas/containers/registro-vuchers/registro-vuchers.component.ts
+++ odonubeFront/src/app/finanzas/containers/registro-vuchers/registro-vuchers.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { FinanzasService } from '../../services/finanzas.service';
 
 @Component({
   selector: 'app-registro-vuchers',
@@ -7,9 +17,125 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroVuchersComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  editMode: number;
+  @Input()
+  usuario: any;
 
-  ngOnInit(): void {
+  @Output()
+  onData = new EventEmitter<any>();
+
+  registroVauche: FormGroup;
+
+  showError: {};
+  errorMsg: string;
+
+  constructor(
+    private auth: AuthService,
+    // private vauches: FinanzasService,
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.initForm();
+    this.showError = {
+      errorAct: false,
+    };
   }
+
+  initForm() {
+    this.registroVauche = new FormGroup({
+      idVauche: new FormControl('', Validators.required),
+      tipo: new FormControl('', Validators.required),
+      namecuenta: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      referencia: new FormControl('', Validators.required),
+      monto: new FormControl('', Validators.required),
+      metodo: new FormControl('', Validators.required),
+      observacion: new FormControl('', Validators.required)
+    });
+  }
+  get fVauche() {
+    return this.registroVauche.controls;
+  }
+
+  async endRegistro() {
+    try {
+      var aux = this.registroVauche.value;
+      let error;
+
+      let vauche = {
+        idVauche: aux.idVauche,
+        tipo: aux.tipo,
+        namecuenta: aux.namecuenta,
+        date: aux.date,
+        referencia: aux.referencia,
+        monto: aux.monto,
+        metodo: aux.metodo,
+        observacion: aux.observacion,
+      };
+
+      error = this.catchVaucheErrors();
+      if (error) {
+        let errorMsg =
+          'Algunos campos son inválidos. Por favor, revise el formulario e intente de nuevo';
+        this.openError(errorMsg);
+      } else {
+        // let response = await this.vauches.create(vauche);
+        // console.log(response);
+        this.router.navigateByUrl("cuentas/lista")
+      }
+    } catch (e: any) {
+      this.openError(e.message);
+    }
+  }
+
+  catchVaucheErrors() {
+    let aux1 = this.fVauche.idVauche.errors
+      ? this.fVauche.idVauche.errors.required
+      : false;
+    let aux2 = this.fVauche.tipo.errors
+      ? this.fVauche.tipo.errors.required
+      : false;
+    let aux3 = this.fVauche.namecuenta.errors
+      ? this.fVauche.namecuenta.errors.required
+      : false;
+    let aux4 = this.fVauche.date.errors
+      ? this.fVauche.date.errors.required
+      : false;
+    let aux5 = this.fVauche.referencia.errors
+      ? this.fVauche.referencia.errors.required
+      : false;
+    let aux6 = this.fVauche.monto.errors
+      ? this.fVauche.monto.errors.required
+      : false;
+    let aux7 = this.fVauche.metodo.errors
+      ? this.fVauche.metodo.errors.required
+      : false;
+    let aux8 = this.fVauche.observacion.errors
+      ? this.fVauche.observacion.errors.required
+      : false;
+    let error = aux1 || aux2 || aux3 || aux4 || aux5 || aux6 || aux7 || aux8;
+    return error;
+  }
+
+  openError(msg: string) {
+    this.errorMsg = msg;
+    this.showError = {
+      errorAct: true,
+    };
+  }
+
+  closeError() {
+    this.showError = {
+      errorAct: false,
+    };
+  }
+
+  resetForms() {
+    this.registroVauche.reset();
+  }
+
 
 }
